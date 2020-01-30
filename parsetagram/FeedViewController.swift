467ac038b10244368b740e6ead4c0411c9ce9423
alectene <alectene@mail.sfsu.edu>
2020-01-30 15:27:56 -0800

    User can now login & sign up through parse

---
+++ parsetagram/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  parsetagram
+//
+//  Created by Alec Tenefrancia on 1/30/20.
+//  Copyright © 2020 Alec Tenefrancia. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
