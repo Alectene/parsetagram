ebb54fa58d7c297ac20db3ca358eb949e8f0a754
alectene <alectene@mail.sfsu.edu>
2020-02-01 15:30:33 -0800

    user can now open camera, apply image to camera view

---
+++ parsetagram/CameraViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CameraViewController.swift
+//  parsetagram
+//
+//  Created by Alec Tenefrancia on 2/1/20.
+//  Copyright © 2020 Alec Tenefrancia. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+//image picker lets you use camera
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmitButton(_ sender: Any) {
+    }
+    // function below here, upon tapping image will open photo album
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }else{
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+        
+    }
+    //function below here will save pic upon choosing
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
